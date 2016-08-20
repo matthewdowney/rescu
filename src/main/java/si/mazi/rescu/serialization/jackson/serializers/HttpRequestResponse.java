58685d3a18eccd13d8a98625662724d319d69c9c
Bryan Hernandez <bryan4887@gmail.com>
2016-08-20 14:01:21 -0500

    Added toString builders to HttpResponse and HttpRequestResponse
    	modified:   src/main/java/si/mazi/rescu/serialization/jackson/serializers/HttpRequestResponse.java
    	modified:   src/main/java/si/mazi/rescu/serialization/jackson/serializers/HttpResponse.java

--- src/main/java/si/mazi/rescu/serialization/jackson/serializers/HttpRequestResponse.java
+++ src/main/java/si/mazi/rescu/serialization/jackson/serializers/HttpRequestResponse.java
@@ -1,30 +1,37 @@
 package si.mazi.rescu.serialization.jackson.serializers;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 public class HttpRequestResponse {
-  private HttpRequest request;
-  private HttpResponse response;
+	private HttpRequest request;
+	private HttpResponse response;
 
-  public HttpRequestResponse() {}
+	public HttpRequestResponse() {}
 
-  public HttpRequestResponse(HttpRequest request, HttpResponse response) {
-    this.request = request;
-    this.response = response;
-  }
+	public HttpRequestResponse(HttpRequest request, HttpResponse response) {
+		this.request = request;
+		this.response = response;
+	}
 
-  public HttpRequest getRequest() {
-    return request;
-  }
+	public HttpRequest getRequest() {
+		return request;
+	}
 
-  public void setRequest(HttpRequest request) {
-    this.request = request;
-  }
+	public void setRequest(HttpRequest request) {
+		this.request = request;
+	}
 
-  public HttpResponse getResponse() {
-    return response;
-  }
+	public HttpResponse getResponse() {
+		return response;
+	}
 
-  public void setResponse(HttpResponse response) {
-    this.response = response;
-  }
+	public void setResponse(HttpResponse response) {
+		this.response = response;
+	}
 
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+	}
 }
--- src/main/java/si/mazi/rescu/serialization/jackson/serializers/HttpResponse.java
+++ src/main/java/si/mazi/rescu/serialization/jackson/serializers/HttpResponse.java
@@ -1,40 +1,48 @@
 package si.mazi.rescu.serialization.jackson.serializers;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 public class HttpResponse {
-  private int status;
-  private String body;
-  private long time;
+	private int status;
+	private String body;
+	private long time;
 
-  public HttpResponse() {}
+	public HttpResponse() {}
 
-  public HttpResponse(int status, String body, long originTimeNanos, long startNano) {
-    this.status = status;
-    this.body = body;
-    this.time = originTimeNanos + (System.nanoTime() - startNano);
-  }
+	public HttpResponse(int status, String body, long originTimeNanos, long startNano) {
+		this.status = status;
+		this.body = body;
+		time = originTimeNanos + (System.nanoTime() - startNano);
+	}
 
-  public int getStatus() {
-    return status;
-  }
+	public int getStatus() {
+		return status;
+	}
 
-  public void setStatus(int status) {
-    this.status = status;
-  }
+	public void setStatus(int status) {
+		this.status = status;
+	}
 
-  public String getBody() {
-    return body;
-  }
+	public String getBody() {
+		return body;
+	}
 
-  public void setBody(String body) {
-    this.body = body;
-  }
+	public void setBody(String body) {
+		this.body = body;
+	}
 
-  public long getTime() {
-    return time;
-  }
+	public long getTime() {
+		return time;
+	}
 
-  public void setTime(long time) {
-    this.time = time;
-  }
+	public void setTime(long time) {
+		this.time = time;
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+	}
 
 }
