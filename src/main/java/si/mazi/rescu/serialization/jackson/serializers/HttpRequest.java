d8de776ec75000ef3436c1ba1ef71da5d704c5fa
Matthew Downey <matthewdowney20@gmail.com>
2016-08-22 22:43:07 -0500

    Formatted & added copy constructor.
    	modified:   src/main/java/si/mazi/rescu/serialization/jackson/serializers/HttpRequest.java

--- src/main/java/si/mazi/rescu/serialization/jackson/serializers/HttpRequest.java
+++ src/main/java/si/mazi/rescu/serialization/jackson/serializers/HttpRequest.java
@@ -7,64 +7,74 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 public class HttpRequest {
-	private String url;
-	private String method;
-	private Map<String, List<String>> headers;
-	private String body;
-	private long time;
+  private String url;
+  private String method;
+  private Map<String, List<String>> headers;
+  private String body;
+  private long time;
 
-	public HttpRequest() {}
+  public HttpRequest() {
+  }
 
-	public HttpRequest(String url, String method, Map<String, List<String>> headers, String body, long originTimeNanos,
-			long startNano) {
-		this.url = url.trim();
-		this.method = method;
-		this.headers = headers;
-		this.body = body;
-		time = originTimeNanos + (System.nanoTime() - startNano);
-	}
+  public HttpRequest(String url, String method, Map<String, List<String>> headers, String body, long originTimeNanos,
+      long startNano) {
+    this.url = url.trim();
+    this.method = method;
+    this.headers = headers;
+    this.body = body;
+    this.time = originTimeNanos + (System.nanoTime() - startNano);
+  }
 
-	public String getUrl() {
-		return url;
-	}
+  public HttpRequest(HttpRequest other) {
+    this.url = other.url;
+    this.method = other.method;
+    this.headers = other.headers;
+    this.body = other.body;
+    this.time = other.time;
+  }
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+  public String getUrl() {
+    return url;
+  }
 
-	public String getMethod() {
-		return method;
-	}
+  public void setUrl(String url) {
+    this.url = url;
+  }
 
-	public void setMethod(String method) {
-		this.method = method;
-	}
+  public String getMethod() {
+    return method;
+  }
 
-	public Map<String, List<String>> getHeaders() {
-		return headers;
-	}
+  public void setMethod(String method) {
+    this.method = method;
+  }
 
-	public void setHeaders(Map<String, List<String>> headers) {
-		this.headers = headers;
-	}
+  public Map<String, List<String>> getHeaders() {
+    return headers;
+  }
 
-	public String getBody() {
-		return body;
-	}
+  public void setHeaders(Map<String, List<String>> headers) {
+    this.headers = headers;
+  }
 
-	public void setBody(String body) {
-		this.body = body;
-	}
+  public String getBody() {
+    return body;
+  }
 
-	public long getTime() {
-		return time;
-	}
+  public void setBody(String body) {
+    this.body = body;
+  }
 
-	public void setTime(long time) {
-		this.time = time;  }
+  public long getTime() {
+    return time;
+  }
 
-	@Override
-	public String toString() {
-		return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
-	}
+  public void setTime(long time) {
+    this.time = time;
+  }
+
+  @Override
+  public String toString() {
+    return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+  }
 }
